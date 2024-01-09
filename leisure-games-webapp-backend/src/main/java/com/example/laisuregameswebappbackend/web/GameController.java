4a99e4d468572c3343696e2fdf6825ecfe9a26b7
Kondratov Ivan <kondratovivanqq@gmail.com>
2024-01-09 17:31:20 +0200

    added Access-Control-Allow-Origin header to response

--- leisure-games-webapp-backend/src/main/java/com/example/laisuregameswebappbackend/web/GameController.java
+++ leisure-games-webapp-backend/src/main/java/com/example/laisuregameswebappbackend/web/GameController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/game")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GameController {
     private final GameService gameService;
 
