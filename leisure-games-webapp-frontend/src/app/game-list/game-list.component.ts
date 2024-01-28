75a03d2b842b53c106e76f156629c6ee3dc1d28b
Kondratov Ivan <kondratovivanqq@gmail.com>
2024-01-28 19:22:29 +0200

    added filter by game category

--- leisure-games-webapp-frontend/src/app/game-list/game-list.component.ts
+++ leisure-games-webapp-frontend/src/app/game-list/game-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from '../game'
-import { GameService } from '../game.service'
-import { FavoriteGamesService } from '../favorite-games.service'; 
+import { Game } from '../game';
+import { GameService } from '../game.service';
+import { FavoriteGamesService } from '../favorite-games.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,26 +9,43 @@ import { Router } from '@angular/router';
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.css']
 })
-export class GameListComponent implements OnInit{
+export class GameListComponent implements OnInit {
 
-  games: Game[]= [];
-  
-  constructor(private gameService: GameService, private favoriteGamesService: FavoriteGamesService, private router: Router){}
+  games: Game[] = [];
+  filteredGames: Game[] = [];
+  selectedCategory: string = 'nothing';
 
-  ngOnInit(): void{ 
+  constructor(private gameService: GameService, private favoriteGamesService: FavoriteGamesService, private router: Router) {}
+
+  ngOnInit(): void {
     this.getGames();
   }
 
-  private getGames(){
+  private getGames() {
     this.gameService.getGamesList().subscribe(data => {
-      this.games=data;
+      this.games = data;
+      this.filteredGames = [...this.games];
     },
     error => {
       console.error('An error occurred while receiving data:', error);
     });
   }
 
-  gameDetails(id: number){
+  onRadioChange(event: Event) {
+    const radio: HTMLInputElement = event.target as HTMLInputElement;
+    this.selectedCategory = radio.value;
+    this.filterGames();
+  }
+
+  filterGames() {
+    if (this.selectedCategory === 'nothing') {
+      this.filteredGames = [...this.games];
+    } else {
+      this.filteredGames = this.games.filter(game => game.category === this.selectedCategory);
+    }
+  }
+
+  gameDetails(id: number) {
     this.router.navigate(['game-details', id]);
   }
 
@@ -43,7 +60,7 @@ export class GameListComponent implements OnInit{
   removeFromFavorites(game: Game) {
     if (this.isInFavorites(game)) {
       this.favoriteGamesService.removeFromFavorites(game);
-      this.games = this.games.filter(g => !this.isInFavorites(g)); // Оставляем только те игры, которые остались в списке избранных
+      this.filteredGames = this.filteredGames.filter(g => !this.isInFavorites(g));
     }
   }
 
@@ -54,5 +71,4 @@ export class GameListComponent implements OnInit{
       this.addToFavorites(game);
     }
   }
-
 }
--- leisure-games-webapp-frontend/src/app/game.ts
+++ leisure-games-webapp-frontend/src/app/game.ts
@@ -5,13 +5,15 @@ export class Game {
     shortDesc:string;
     fullDesc:string;
     imageLink:string;
+    category:string;
 
-    constructor(id?: number, name?: string, fame?: string, shortDesc?:string, fullDesc?: string, imageLink?: string) {
+    constructor(id?: number, name?: string, fame?: string, shortDesc?:string, fullDesc?: string, imageLink?: string, category?: string) {
         this.id = id || 0;
         this.name = name || '';
         this.fame = fame || '';
         this.shortDesc = shortDesc || '';
         this.fullDesc = fullDesc || '';
         this.imageLink = imageLink || '';
+        this.category = category || '';
     }
 }
